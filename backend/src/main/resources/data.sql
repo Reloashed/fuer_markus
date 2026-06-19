443001054e9c8b17b50d2b918986c6c3888fbef5
Reloashed <remscherr@gmail.com>
2026-06-19 11:12:38 +0200

    super duper mega cleane fix

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO user_profile (id, first_name, last_name, address, profile_img_url, profile_status)
+INSERT INTO user_profile (first_name, last_name, address, profile_img_url, profile_status)
 VALUES
-    (1, 'Admin', 'User', 'Admin Street 1', NULL, 'UNVERIFIED'),
-    (2, 'Anna', 'Müller', 'Berlin 12', NULL, 'UNVERIFIED'),
-    (3, 'James', 'Bond', 'London HQ', NULL, 'UNVERIFIED');
+    ('Admin', 'User', 'Admin Street 1', NULL, 'UNVERIFIED'),
+    ('Anna', 'Müller', 'Berlin 12', NULL, 'UNVERIFIED'),
+    ('James', 'Bond', 'London HQ', NULL, 'UNVERIFIED');
 
 -- Initial data for account table
 -- Passwords are Bcrypt hashes of the password 'pizza'.
@@ -13,14 +13,14 @@ INSERT INTO account (first_name, last_name, username, password_hash, email, role
 
 INSERT INTO groups (id, name, owner_id)
 VALUES
-    (1, 'Family', 1),
-    (2, 'Friends', 2);
+    ('Family', 1),
+    ('Friends', 2);
 
 INSERT INTO media (id, media, group_id)
 VALUES
-    (1, 'family_photo.jpg', 1),
-    (2, 'vacation_video.mp4', 1),
-    (3, 'party_photo.png', 2);
+    ('family_photo.jpg', 1),
+    ('vacation_video.mp4', 1),
+    ('party_photo.png', 2);
 
 INSERT INTO members_groups (group_id, member_id)
 VALUES
@@ -29,5 +29,3 @@ VALUES
     (1, 3),
     (2, 2),
     (2, 3);
-
-ALTER TABLE user_profile ALTER COLUMN id RESTART WITH 100;
